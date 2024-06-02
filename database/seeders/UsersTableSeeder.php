bee92c5c6fcfcfdba9b4fc6d3249da76293090bb
Henry Lee <henryleeworld@gmail.com>
2024-06-03 00:17:19 +0800

    feat:更新最新資料

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -14,7 +15,7 @@ class UsersTableSeeder extends Seeder
                 'id'             => 1,
                 'name'           => 'Admin',
                 'email'          => 'admin@admin.com',
-                'password'       => '$2y$10$rA.UbFkvBY0TnPXC5AvdEew7ITCs.PFo9eAfkH.m48GlOdhFMXvLG',
+                'password'       => Hash::make('password'),
                 'remember_token' => null,
                 'created_at'     => '2020-10-08 06:09:29',
                 'updated_at'     => '2020-10-08 06:09:29',
